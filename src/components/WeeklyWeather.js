3508f11807483f53faf4ad6b2506219cb6d52a98
ZicoRazzi <zicorazzi@gmail.com>
2022-01-31 15:50:52 +0100

    api solved

--- src/components/WeeklyWeather.js
+++ src/components/WeeklyWeather.js
@@ -34,99 +34,12 @@ export default function WeeklyWeather({ weather, weatherDetails }) {
   return (
     <WeeklyWeatherStyle>
       <div className="weather_container">
-<<<<<<< HEAD
       {weatherDetails.daily.map((day, i) => {
           if (i === 0 || i >= 8) return;
           return (
             <AccordionComp key={i + Math.random()} weatherDataPerDay={day} />
           );
         })}
-=======
-        <AccordionComp
-          today={dateBuilderAccordion(
-            new Date(weatherDetails.daily[1].dt * 1000)
-          )}
-          src={`https://openweathermap.org/img/wn/${weatherDetails.daily[1].weather[0].icon}.png`}
-          min_temp={Math.round(weatherDetails.daily[1].temp.min - 273)}
-          max_temp={Math.round(weatherDetails.daily[1].temp.max - 273)}
-          morning_temp={Math.round(weatherDetails.daily[1].temp.morn - 273)}
-          afternoon_temp={Math.round(weatherDetails.daily[1].temp.day - 273)}
-          evening_temp={Math.round(weatherDetails.daily[1].temp.eve - 273)}
-          night_temp={Math.round(weatherDetails.daily[1].temp.night - 273)}
-        />
-        <AccordionComp
-          today={dateBuilderAccordion(
-            new Date(weatherDetails.daily[2].dt * 1000)
-          )}
-          src={`https://openweathermap.org/img/wn/${weatherDetails.daily[2].weather[0].icon}.png`}
-          min_temp={Math.round(weatherDetails.daily[2].temp.min - 273)}
-          max_temp={Math.round(weatherDetails.daily[2].temp.max - 273)}
-          morning_temp={Math.round(weatherDetails.daily[2].temp.morn - 273)}
-          afternoon_temp={Math.round(weatherDetails.daily[2].temp.day - 273)}
-          evening_temp={Math.round(weatherDetails.daily[2].temp.eve - 273)}
-          night_temp={Math.round(weatherDetails.daily[2].temp.night - 273)}
-        />
-        <AccordionComp
-          today={dateBuilderAccordion(
-            new Date(weatherDetails.daily[3].dt * 1000)
-          )}
-          src={`https://openweathermap.org/img/wn/${weatherDetails.daily[3].weather[0].icon}.png`}
-          min_temp={Math.round(weatherDetails.daily[3].temp.min - 273)}
-          max_temp={Math.round(weatherDetails.daily[3].temp.max - 273)}
-          morning_temp={Math.round(weatherDetails.daily[3].temp.morn - 273)}
-          afternoon_temp={Math.round(weatherDetails.daily[3].temp.day - 273)}
-          evening_temp={Math.round(weatherDetails.daily[3].temp.eve - 273)}
-          night_temp={Math.round(weatherDetails.daily[3].temp.night - 273)}
-        />
-        <AccordionComp
-          today={dateBuilderAccordion(
-            new Date(weatherDetails.daily[4].dt * 1000)
-          )}
-          src={`https://openweathermap.org/img/wn/${weatherDetails.daily[4].weather[0].icon}.png`}
-          min_temp={Math.round(weatherDetails.daily[4].temp.min - 273)}
-          max_temp={Math.round(weatherDetails.daily[4].temp.max - 273)}
-          morning_temp={Math.round(weatherDetails.daily[4].temp.morn - 273)}
-          afternoon_temp={Math.round(weatherDetails.daily[4].temp.day - 273)}
-          evening_temp={Math.round(weatherDetails.daily[4].temp.eve - 273)}
-          night_temp={Math.round(weatherDetails.daily[4].temp.night - 273)}
-        />
-        <AccordionComp
-          today={dateBuilderAccordion(
-            new Date(weatherDetails.daily[5].dt * 1000)
-          )}
-          src={`https://openweathermap.org/img/wn/${weatherDetails.daily[5].weather[0].icon}.png`}
-          min_temp={Math.round(weatherDetails.daily[5].temp.min - 273)}
-          max_temp={Math.round(weatherDetails.daily[5].temp.max - 273)}
-          morning_temp={Math.round(weatherDetails.daily[5].temp.morn - 273)}
-          afternoon_temp={Math.round(weatherDetails.daily[5].temp.day - 273)}
-          evening_temp={Math.round(weatherDetails.daily[5].temp.eve - 273)}
-          night_temp={Math.round(weatherDetails.daily[5].temp.night - 273)}
-        />
-        <AccordionComp
-          today={dateBuilderAccordion(
-            new Date(weatherDetails.daily[6].dt * 1000)
-          )}
-          src={`https://openweathermap.org/img/wn/${weatherDetails.daily[6].weather[0].icon}.png`}
-          min_temp={Math.round(weatherDetails.daily[6].temp.min - 273)}
-          max_temp={Math.round(weatherDetails.daily[6].temp.max - 273)}
-          morning_temp={Math.round(weatherDetails.daily[6].temp.morn - 273)}
-          afternoon_temp={Math.round(weatherDetails.daily[6].temp.day - 273)}
-          evening_temp={Math.round(weatherDetails.daily[6].temp.eve - 273)}
-          night_temp={Math.round(weatherDetails.daily[6].temp.night - 273)}
-        />
-        <AccordionComp
-          today={dateBuilderAccordion(
-            new Date(weatherDetails.daily[7].dt * 1000)
-          )}
-          src={`https://openweathermap.org/img/wn/${weatherDetails.daily[7].weather[0].icon}.png`}
-          min_temp={Math.round(weatherDetails.daily[7].temp.min - 273)}
-          max_temp={Math.round(weatherDetails.daily[7].temp.max - 273)}
-          morning_temp={Math.round(weatherDetails.daily[7].temp.morn - 273)}
-          afternoon_temp={Math.round(weatherDetails.daily[7].temp.day - 273)}
-          evening_temp={Math.round(weatherDetails.daily[7].temp.eve - 273)}
-          night_temp={Math.round(weatherDetails.daily[7].temp.night - 273)}
-        />
->>>>>>> a2e638dcdf24a51e06a48ce7a72c442aa91d6fa0
       </div>
     </WeeklyWeatherStyle>
   );
