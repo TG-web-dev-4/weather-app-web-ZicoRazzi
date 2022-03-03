e016fd39c9236f588b73281f292f954a90b45d58
ZicoRazzi <zicorazzi@gmail.com>
2022-03-03 15:34:34 +0100

    router update

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HushRouter as Router } from 'react-router-dom';
+import { HushRouter } from 'react-router-dom';
 import App from './App';
 import GlobalStyles from './styles/GlobalStyles';
 import Typography from './styles/Typography';
 
 ReactDOM.render(
-  <Router>
+  <HushRouter>
     <GlobalStyles />
     <Typography />
     <App />
-  </Router>,
+  </HushRouter>,
   document.getElementById('root')
 );
