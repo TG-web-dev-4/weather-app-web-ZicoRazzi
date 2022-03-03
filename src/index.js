554df1be85a0e0c911ed58aa1fcf3541cc53f8c1
ZicoRazzi <zicorazzi@gmail.com>
2022-03-03 15:51:44 +0100

    Configure React app for deployment to GitHub Pages

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HushRouter } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import App from './App';
 import GlobalStyles from './styles/GlobalStyles';
 import Typography from './styles/Typography';
 
 ReactDOM.render(
-  <HushRouter>
+  <Router>
     <GlobalStyles />
     <Typography />
     <App />
-  </HushRouter>,
+  </Router>,
   document.getElementById('root')
 );
